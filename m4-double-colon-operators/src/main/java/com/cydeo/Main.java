cee50203525f98a1d266dc0a82e55044f934c736
Bilal Ghani <bghani1995@gmail.com>
2022-08-19 11:00:11 -0500

    Double Colon Operators

--- m4-double-colon-operators/src/main/java/com/cydeo/Main.java
+++ m4-double-colon-operators/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Main {
@@ -9,6 +10,25 @@ public class Main {
         //  Predicate<Integer> pred = (p) -> p%2 == 0 ? true : false;// implementation for abstract method that belongs to that functional interface
 
         Calculate s1 = (x,y) -> Calculator.findSum(x,y);
+
+        //Refers to Static Method - static method can be called through class name
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10, 20);
+
+        //Refers to an Instance Method - instance method can only be called through object
+        Calculator obj = new Calculator();
+        Calculate s3 = obj::findMultiply;
+        s3.calculate(4, 2);
+
+        // Calculate s4 = new Calculator() ::findMultiply;
+
+        BiFunction<String, Integer, String> func = (str, i) -> str.substring(i);
+
+        BiFunction<String, Integer, String> func2 = String::substring;
+
+
+
+
 
     }
 }
