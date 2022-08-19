efc4872b07c93ac8bbd5d9a19813a8adc138be61
Bilal Ghani <bghani1995@gmail.com>
2022-08-19 10:45:27 -0500

    Double Colon Operators

--- m4-double-colon-operators/src/main/java/com/cydeo/Main.java
+++ m4-double-colon-operators/src/main/java/com/cydeo/Main.java
@@ -1,10 +1,14 @@
 package com.cydeo;
 
+import java.util.function.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //  Predicate<Integer> pred = (p) -> p%2 == 0 ? true : false;// implementation for abstract method that belongs to that functional interface
 
+        Calculate s1 = (x,y) -> Calculator.findSum(x,y);
 
     }
 }
