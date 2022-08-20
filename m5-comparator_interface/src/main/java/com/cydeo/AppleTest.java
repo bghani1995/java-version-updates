b27c8b61881030eef01ae42fe6ec26f68a1c5a83
Bilal Ghani <bghani1995@gmail.com>
2022-08-20 01:43:16 -0500

    Stream

--- m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -24,6 +24,18 @@ public class AppleTest {
         inventory.sort(comparing(Apple::getWeight));
         System.out.println(inventory);
 
+        //Reversed
+        inventory.sort(comparing(Apple::getWeight).reversed());
+        System.out.println(inventory);
+
+        //Chaining
+        inventory
+                .sort(comparing(Apple::getWeight)//Compare based on the weight
+                        .reversed()// sort it
+                        .thenComparing(Apple::getColor));// then compare based on the color
+
+
+
 
 
 
