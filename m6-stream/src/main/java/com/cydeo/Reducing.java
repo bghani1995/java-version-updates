ccc497f4be2354fcab095de0f88a4eaad605f4c3
Bilal Ghani <bghani1995@gmail.com>
2022-08-21 11:50:03 -0500

    Terminal Operations

--- m6-stream/src/main/java/com/cydeo/Reducing.java
+++ m6-stream/src/main/java/com/cydeo/Reducing.java
@@ -1,4 +1,22 @@
 package com.cydeo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Reducing {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(4,5,3,9);
+
+        int sum = 0;
+        for(int number : numbers){
+            sum = sum += number;
+        }
+
+        //Functional Programming
+        int result = numbers.stream().reduce(0, (a,b) -> a+b);
+        System.out.println(result);
+
+    }
 }
