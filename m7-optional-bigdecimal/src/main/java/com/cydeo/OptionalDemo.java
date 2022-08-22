ce169d9e63c437e95b24bc319e589f23885bab79
Bilal Ghani <bghani1995@gmail.com>
2022-08-22 10:34:49 -0500

    Optional Type

--- m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -1,4 +1,23 @@
 package com.cydeo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2,4,3,10,23);
+
+        //empty()- isPresent()
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        System.out.println(empty.isPresent());
+
+        System.out.println(Optional.of(number).isPresent());
+
+
+
+    }
 }
