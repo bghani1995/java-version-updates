a786e1bba479ba71c478bbfdc2e200cf1e89e8ae
Bilal Ghani <bghani1995@gmail.com>
2022-08-22 11:27:32 -0500

    Big Decimal

--- m7-optional-bigdecimal/src/main/java/com/cydeo/BigDecimalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/BigDecimalDemo.java
@@ -11,9 +11,20 @@ public class BigDecimalDemo {
 
         BigDecimal b1 = new BigDecimal("374.56");
         BigDecimal b2 = new BigDecimal("374.26");
+        BigDecimal b3 = BigDecimal.valueOf(374.56);
+        BigDecimal b4 = BigDecimal.TEN;
+        BigDecimal b5 = BigDecimal.ONE;
+
         System.out.println(b1.subtract(b2));
 
 
 
     }
+}
+
+class Employee{
+    private String name;
+    private BigDecimal salary;
+//    private double salary;
+
 }
